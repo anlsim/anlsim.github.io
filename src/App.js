2de9242e8a92fa97ace1638798fc57a57e711998
anlsim <anlsimental@gmail.com>
2021-02-07 22:16:04 -0800

    Adding React Router / NavBar links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router,
+Route,
+} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/NavBar';
-import HomaPage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
+import ResumePage from './pages/ContactPage';
+
 function App() {
   return (
     <>
-    <NavBar />
+    <Router>
+      <div>
+          <NavBar />
+      </div>
+      <div>
+          <Route path="/" component= {HomePage} exact/>
+          <Route path="/resume" component= {ResumePage} />
+          <Route path="/projects" component= {ProjectsPage} />
+          <Route path="/contact" component= {ContactPage} />
+      </div>
+      
+    </Router>
+    
     <div></div>
    </>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
 
     return (
         <nav className ="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">AS</a>
+        <Link to="/">Home</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Resume</a>
+                <Link to="/Resume">Resume</Link>
+               
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Projects</a>
+              <Link to="/projects">Projects</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+              <Link to="/contact">Contact</Link>
+                
               </li>
               
               
