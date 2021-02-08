89ad610f7b691c484c1d0bf04215bc1c05b69767
anlsim <anlsimental@gmail.com>
2021-02-07 22:03:55 -0800

    Adding Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from './components/NavBar';
+import HomaPage from './pages/HomePage';
+import ProjectsPage from './pages/ProjectsPage';
+import ContactPage from './pages/ContactPage';
 function App() {
   return (
     <>
     <NavBar />
-   
+    <div></div>
    </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ const NavBar = () => {
                 <a className="nav-link" href="#">Projects</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Education</a>
+                <a className="nav-link" href="#">Contact</a>
               </li>
               
               
