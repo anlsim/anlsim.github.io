096ac2bfa3ae1034b5b1ac149c3cd8822e45839c
anlsim <anlsimental@gmail.com>
2021-02-11 23:34:02 -0800

    adding style to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,9 @@ import DirectoryMenu from '../components/DirectoryMenu';
 const HomePage = () => {
     return (
         <div className = 'homepage'>
+            <div className= 'intro'>
+                <h1 className= 'intro-text'> Hi, I am Andrea</h1>
+            </div>
             <DirectoryMenu/>
             
         </div>
