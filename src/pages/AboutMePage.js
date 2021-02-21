13075d6990115e7587e429e8849da76366b16d9a
anlsim <anlsimental@gmail.com>
2021-02-20 20:15:38 -0800

    making homepage responsive

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import SectionTitle from '../components/SectionTitle';
 import NavBar from '../components/NavBar';
+import '../styles/aboutme.scss';
 
 const AboutMePage = (props) => {
     return (
       <>
       <NavBar />
-      <SectionTitle  title={props.title}/>
-      <div className="container pt-2">
-       
-        <div className="container bg-primary pt-5">
-          <div className="row center">
-              <div className="col"><img src="" alt=""></img></div>
-              <div className="col">
+      <SectionTitle  title={props.title}/>  
+        <div className="container pt-5">
+          <div className="about">
+              <div className="img"><img src="" alt=""></img></div>
+              <div className="content">
                 <h4>Collaborative and creative Software Developer with a keen interest in building responsive and scalable web apps to enhance user experience. Fluent in Spanish and English, accustomed to working with cross-cultural, global teams. </h4>
               </div>
           </div>
         </div>
         <br/>
-        <div className="container  bg-light pt-5">Education</div>
-      </div>
+        <div className="container pt-5">
+          <h2>Education</h2>
+          <div className ='jumbotron'>1</div>
+          <div className ='jumbotron'>2</div>
+          <div className ='jumbotron'>3</div>
+        </div>
+      
       </>
       
        
