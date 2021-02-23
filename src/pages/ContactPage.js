8c9511dc770ee33c15c5a3773dc13a3407f9bf74
anlsim <anlsimental@gmail.com>
2021-02-22 19:57:57 -0800

    stylying contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faInstagram} from '@fortawesome/free-brands-svg-icons';
 import SectionTitle from '../components/SectionTitle';
 import NavBar from '../components/NavBar';
+import '../styles/ContacPage.scss';
 
 const ContactPage = (props) => {
     return (
@@ -12,23 +13,28 @@ const ContactPage = (props) => {
      
         <SectionTitle title={props.title}/>
         <div className="container">
-
-            <div className="row">
-                       
+            <div className="row">                 
                 <div className="col">
-                    <h1>Contact Me</h1>
-                    <h2>
-                        <a href="https://github.com/anlsim" target="blank">
+                    <h2 className='contactIcon'>
+                        <a className='contactIcon-color' href="https://github.com/anlsim" target="blank">
                             <FontAwesomeIcon icon={faGithub}/>
                         </a>
                     </h2>
-                    <h2>
-                        <a href="https://www.linkedin.com/in/andrea-simental/" target="blank">
+                </div>
+                <div className="col">
+                    <h2 className='contactIcon'>
+                        <a className='contactIcon-color' href="https://www.linkedin.com/in/andrea-simental/" target="blank">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
-                    </h2>
-                                               
+                    </h2 >
                 </div>
+                <div className="col">
+                    <h2 className='contactIcon'>
+                        <a  className='contactIcon-color' href="https://www.instagram.com/andrealsimental/" target="blank">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </a>
+                    </h2>
+                </div>                                              
             </div>
         </div>
     </>
