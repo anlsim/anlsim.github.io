04354f9867860a2533588e39d12b26df766719f7
anlsim <anlsimental@gmail.com>
2021-02-23 11:29:57 -0800

    updating responsive settings for phone

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -24,21 +24,21 @@ const AboutMePage = (props) => {
        
         <br/>
         <div className="container pt-5">
-          <h1 className='education-title'>Education</h1>
-          <div className ='jumbotron'>
-            <h2>BACHELOR’S OF APPLIED SCIENCE DEGREE • 2019 - 2021</h2>
-            <h3>COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.71</h3>
-            <h4>Lake Washington Institute of Technology • Seattle, WA</h4>
+          <h1 className="education-title">Education</h1>
+          <div className ="jumbotron">
+            <h2 className="degree">BACHELOR’S OF APPLIED SCIENCE DEGREE • 2019 - 2021</h2>
+            <h3 className="degree-title">COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.71</h3>
+            <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
-          <div className ='jumbotron'>
-            <h2>ASSOCIATE’S OF APPLIED SCIENCE • 2017 - 2019</h2>
-            <h3>COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.68</h3>
-            <h4>Lake Washington Institute of Technology • Seattle, WA</h4>
+          <div className ="jumbotron">
+            <h2 className="degree">ASSOCIATE’S OF APPLIED SCIENCE • 2017 - 2019</h2>
+            <h3 className="degree-title">COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.68</h3>
+            <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
-          <div className ='jumbotron'>
-            <h2>BACHELOR’S DEGREE • 2007 - 2011</h2>
-            <h3>GRAPHIC DESIGN • GPA: 3.80</h3>
-            <h4>Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
+          <div className ="jumbotron">
+            <h2 className="degree">BACHELOR’S DEGREE • 2007 - 2011</h2>
+            <h3 className="degree-title">GRAPHIC DESIGN • GPA: 3.80</h3>
+            <h4 className="degree-address">Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
           </div>
         </div>
       
