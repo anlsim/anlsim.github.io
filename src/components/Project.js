a4f8b09f982d11c3a7c8180e25cd102fb99bcde1
anlsim <anlsimental@gmail.com>
2021-03-05 20:56:56 -0800

    updating projects page styles

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,26 +10,20 @@ const Project = ({title, description, linkUrl, imgUrl, gitUrl, tools}) => (
             <div className="row-md-1">
                 <div className = 'card'>
                     <div className='card-title'>
-                    <h1 className ='card-name'>{title}</h1>
+                        <h1 className ='card-name'>{title}</h1>
                     </div>
                     <div className='card-body'>
-                    <p className ='card-description'>{description}</p>
-                    <hr/>
-                    <h2 className='card-tools'>Tools used:</h2>
-                    <p className ='card-description'>{tools}</p>
-        
-                    </div>
-               
-                    
+                        <p className ='card-description'>{description}</p>
+                        <hr/>
+                        <h2 className='card-tools'>Tools used:</h2>
+                        <p className ='card-description'>{tools}</p>
+                    </div>  
                     <div className= 'card-footer'>
-                        
-                            <h3>
-                                <a className="card-footer-icon"  href={gitUrl} target="blank">
-                                <FontAwesomeIcon icon={faGithub}/>
-                                </a>
-                            </h3>                 
-                       
-                        
+                        <h3 className='card-footer-content'>
+                            <a className="card-footer-icon"  href={gitUrl} target="blank">
+                            <FontAwesomeIcon icon={faGithub}/>
+                            </a>
+                        </h3>                           
                     </div>
                 
                 </div>
