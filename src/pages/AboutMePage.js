b29095798c48953eae4174eca108efd96fb4d97d
anlsim <anlsimental@gmail.com>
2021-03-05 19:54:06 -0800

    upating about me styles

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -14,7 +14,7 @@ const AboutMePage = (props) => {
           <span className="img"></span>
         </div>
           <div className="aboutInfo col-sm-8">
-            <h1 className="aboutInfo-title">ANDREA SIMENTAL</h1>
+            <h1 className="aboutInfo-title">Andrea Simental</h1>
             <p className="aboutInfo-description">Collaborative and creative Software Developer with a keen interest in building responsive and scalable web apps to enhance user experience. Fluent in Spanish and English, accustomed to working with cross-cultural, global teams.</p>
 
           </div>
@@ -27,17 +27,17 @@ const AboutMePage = (props) => {
           <h1 className="education-title">Education</h1>
           <div className ="jumbotron">
             <h2 className="degree">BACHELOR’S OF APPLIED SCIENCE DEGREE • 2019 - 2021</h2>
-            <h3 className="degree-title">COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.71</h3>
+            <h3 className="degree-title">Computing and Software Development • GPA: 3.71</h3>
             <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
           <div className ="jumbotron">
             <h2 className="degree">ASSOCIATE’S OF APPLIED SCIENCE • 2017 - 2019</h2>
-            <h3 className="degree-title">COMPUTING AND SOFTWARE DEVELOPMENT • GPA: 3.68</h3>
+            <h3 className="degree-title">Computing and Software Development • GPA: 3.68</h3>
             <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
           <div className ="jumbotron">
             <h2 className="degree">BACHELOR’S DEGREE • 2007 - 2011</h2>
-            <h3 className="degree-title">GRAPHIC DESIGN • GPA: 3.80</h3>
+            <h3 className="degree-title">Graphic Design • GPA: 3.80</h3>
             <h4 className="degree-address">Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
           </div>
         </div>
