094c94fa8c7d2bfe23c5bf45af85bf74c4635306
anlsim <anlsimental@gmail.com>
2021-08-25 22:36:07 -0700

    Refactoring aboutme page

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -8,14 +8,14 @@ const AboutMePage = (props) => {
       <>
       <NavBar />
       <SectionTitle  title={props.title}/>  
-      <div className="aboutMe">
+      <div className="about-me">
         <div className="row">
         <div className="img col-sm-4">
           <span className="img"></span>
         </div>
-          <div className="aboutInfo col-sm-8">
-            <h1 className="aboutInfo-title">Andrea Simental</h1>
-            <p className="aboutInfo-description">Collaborative and creative Software Developer with a keen interest in building responsive and scalable web apps to enhance user experience. Fluent in Spanish and English, accustomed to working with cross-cultural, global teams.</p>
+          <div className="about-me__info col-sm-8">
+            <h1 className="about-me__info--title">Andrea Simental</h1>
+            <p className="about-me__info--description">Collaborative and creative Software Developer with a keen interest in building responsive and scalable web apps to enhance user experience. Fluent in Spanish and English, accustomed to working with cross-cultural, global teams.</p>
 
           </div>
           
@@ -26,16 +26,16 @@ const AboutMePage = (props) => {
         <div className="container pt-5">
           <h1 className="education-title">Experience</h1>
           <div className ="jumbotron">
-            <h2 className="degree">Junior Developer • July  - PRESENT</h2>
-            <h3 className="degree-title">D2 Creative• Full Time</h3>
-            <h4 className="degree-address">
+            <h2 className="degree">Junior Developer • July  - Present</h2>
+            <h3 className="degree__title">D2 Creative• Full Time</h3>
+            <h4 className="degree__address">
               Currently developing websites on a variety of platforms.
             </h4>
           </div>
           <div className ="jumbotron">
             <h2 className="degree">Frontend Developer • March  - July 2021</h2>
-            <h3 className="degree-title">Impakter • Internship</h3>
-            <h4 className="degree-address">
+            <h3 className="degree__title">Impakter • Internship</h3>
+            <h4 className="degree__address">
               Design and develop the frontend of the
               Certificate application while utilizing reusable code
               with Vue.js and Bootstrap. to complete projects in the future.
@@ -52,18 +52,18 @@ const AboutMePage = (props) => {
           <h1 className="education-title">Education</h1>
           <div className ="jumbotron">
             <h2 className="degree">BACHELOR’S OF APPLIED SCIENCE DEGREE • 2019 - 2021</h2>
-            <h3 className="degree-title">Computing and Software Development • GPA: 3.71</h3>
-            <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
+            <h3 className="degree__title">Computing and Software Development • GPA: 3.71</h3>
+            <h4 className="degree__address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
           <div className ="jumbotron">
             <h2 className="degree">ASSOCIATE’S OF APPLIED SCIENCE • 2017 - 2019</h2>
-            <h3 className="degree-title">Computing and Software Development • GPA: 3.68</h3>
-            <h4 className="degree-address">Lake Washington Institute of Technology • Seattle, WA</h4>
+            <h3 className="degree__title">Computing and Software Development • GPA: 3.68</h3>
+            <h4 className="degree__address">Lake Washington Institute of Technology • Seattle, WA</h4>
           </div>
           <div className ="jumbotron">
             <h2 className="degree">BACHELOR’S DEGREE • 2007 - 2011</h2>
-            <h3 className="degree-title">Graphic Design • GPA: 3.80</h3>
-            <h4 className="degree-address">Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
+            <h3 className="degree__title">Graphic Design • GPA: 3.80</h3>
+            <h4 className="degree__address">Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
           </div>
         </div>
      
