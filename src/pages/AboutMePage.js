3f6a45d52f66972cc0050e3720e23c74bb00756d
anlsim <anlsimental@gmail.com>
2021-10-11 22:20:47 -0700

    edited about me

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -8,34 +8,35 @@ const AboutMePage = (props) => {
       <>
       <NavBar />
       <SectionTitle  title={props.title}/>  
-      <div className="about-me">
-        <div className="row">
-        <div className="img col-sm-4">
-          <span className="img"></span>
+      <div className="aboutme">
+          <div className="aboutme__img">
+            {/* <span className="img"></span> */}
+          </div>
+          <div className="aboutme__info">
+            <h1 className="aboutme__info--title">Andrea Simental</h1>
+            <p className="aboutme__info--description">
+              Collaborative and creative Software Developer with 
+              a keen interest in building responsive and scalable
+               web apps to enhance user experience. Fluent in 
+               Spanish and English, accustomed to working with 
+               cross-cultural, global teams.
+            </p>
+          </div>
+      </div>
+      <br/>
+      <div className="container pt-5">
+        <h1 className="education-title">Experience</h1>
+        <div className ="jumbotron">
+          <h2 className="degree">Junior Developer • July  - Present</h2>
+          <h3 className="degree__title">D2 Creative• Full Time</h3>
+          <h4 className="degree__address">
+            Currently developing websites on a variety of platforms.
+          </h4>
         </div>
-          <div className="about-me__info col-sm-8">
-            <h1 className="about-me__info--title">Andrea Simental</h1>
-            <p className="about-me__info--description">Collaborative and creative Software Developer with a keen interest in building responsive and scalable web apps to enhance user experience. Fluent in Spanish and English, accustomed to working with cross-cultural, global teams.</p>
-
-          </div>
-          
-      </div>
-      </div>
-       
-        <br/>
-        <div className="container pt-5">
-          <h1 className="education-title">Experience</h1>
-          <div className ="jumbotron">
-            <h2 className="degree">Junior Developer • July  - Present</h2>
-            <h3 className="degree__title">D2 Creative• Full Time</h3>
-            <h4 className="degree__address">
-              Currently developing websites on a variety of platforms.
-            </h4>
-          </div>
-          <div className ="jumbotron">
-            <h2 className="degree">Frontend Developer • March  - July 2021</h2>
-            <h3 className="degree__title">Impakter • Internship</h3>
-            <h4 className="degree__address">
+        <div className ="jumbotron">
+          <h2 className="degree">Frontend Developer • March  - July 2021</h2>
+          <h3 className="degree__title">Impakter • Internship</h3>
+          <h4 className="degree__address">
               Design and develop the frontend of the
               Certificate application while utilizing reusable code
               with Vue.js and Bootstrap. to complete projects in the future.
@@ -45,9 +46,9 @@ const AboutMePage = (props) => {
               •	Created UI of the certificate tool to detect environmental or social impact for companies.<br/>
               •	Transformed business and functional needs into documented technical
                specifications in collaboration with team members.
-            </h4>
-          </div>
+          </h4>
         </div>
+      </div>
         <div className="container pt-5">
           <h1 className="education-title">Education</h1>
           <div className ="jumbotron">
@@ -66,13 +67,7 @@ const AboutMePage = (props) => {
             <h4 className="degree__address">Universidad de Guadalajara - CUAAD • Guadalajara, MX</h4>
           </div>
         </div>
-     
-      
       </>
-      
-       
-         
-        
     )
 }
 
