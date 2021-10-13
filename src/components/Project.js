0f73235e3b7951c52c297c717594622a98a60164
anlsim <anlsimental@gmail.com>
2021-10-12 21:32:15 -0700

    Fixed project for mobile version

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,49 +9,40 @@ import { faDesktop } from '@fortawesome/free-solid-svg-icons';
 
 const Project = ({title, description, linkUrl, imgUrl, gitUrl, tools}) => (
    
-        <>
             <div className="project">
-                <div className ='card'>
-                    <div className='card__title'>
-                        <h1 className ='card__name'>{title}</h1>
+                    <div className='project__title'>
+                        <h1 className ='project__name'>{title}</h1>
                     </div>
-                    <div className='card__body'>
-                        <p className ='card__description'>{description}</p>
+                    <div className='project__body'>
+                        <p className ='project__description'>{description}</p>
                         <hr/>
-                        <h2 className='card__tools'>Tools used:</h2>
-                        <div className="card__toolsDiv">
-
-                        
-                        {tools.map((t, index) => {
-                            return(
-                                <>
-                                 <p className="card__toolsDiv__description">{t.name} </p>
-                                <i className={t.fAwasome}/>
-                                
-                                </>
-                        // </p>
-                            )
-                        })}
+                        <h2 className='project__section-title'>Tools used</h2>
+                        <div className="project__tools">
+                            {tools.map((t, index) => {
+                                return(
+                                    <>
+                                    <p className="project__tools__desc">{t.name} </p>
+                                    <i className={t.fAwasome}/>
+                                    </>
+                                )
+                            })}
                         </div>
                     </div>  
-                    <div className= 'card__footer'>
-                        <h3 className='card__footer__content'>
-                            <a className="card__footer__icon"  href={gitUrl} target="blank">
+                    <div className= 'project__footer'>
+                            <a className="project__footer__icon"  href={gitUrl} target="blank">
                             <FontAwesomeIcon icon={faGithub}/>
                             </a>
                             {
                             linkUrl && 
-                            <a className="card__footer__icon"  href={linkUrl} target="blank">
+                            <a className="project__footer__icon"  href={linkUrl} target="blank">
                             <FontAwesomeIcon icon={faDesktop}/>
                             </a>
-                            }
-                         
-                        </h3>                           
+                            }               
                     </div>
                 
-                </div>
+            
             </div>
-        </>
+      
 
         
 );
