13ad44b040cd4277bea0092312c3ae1a220bcaf1
anlsim <anlsimental@gmail.com>
2022-01-15 20:43:08 -0800

    Missing onclicks

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,23 +12,23 @@ export default function SideBar({
       {clicked && (
         <ul className='sideBar__menu active'>
           <li className="navBar__menu__links">
-            <Link to="/About">
+            <Link to="/About" onClick={handleClicked}>
               About Me
             </Link>
           </li>
           <li className="navBar__menu__links">
-            <Link to="/projects">
+            <Link to="/projects" onClick={handleClicked}>
               Projects
             </Link>
           </li>
           <li className="navBar__menu__links">
-            <Link to="/Skills">
+            <Link to="/Skills" onClick={handleClicked}>
               Skills
             </Link>
           </li>
 
           <li className="navBar__menu__links">
-            <Link to="/contact">
+            <Link to="/contact" onClick={handleClicked}>
               Contact
             </Link>
           </li> 
