1af62469e3910df7c3a01e1a3c667d493ac58cbb
anlsim <anlsimental@gmail.com>
2022-07-30 21:45:06 -0700

    Updated app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import './App.scss'
 import {
   BrowserRouter as Router,
@@ -12,86 +11,19 @@ import ContactPage from './pages/ContactPage'
 import SkillsPage from './pages/SkillsPage'
 import AboutMePage from './pages/AboutMePage'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      title: 'Andrea Simental',
-      headerLinks: [
-        { title: 'Home', path: '/' },
-        { title: 'About Me', path: '/about' },
-        { title: 'Projects', path: '/projecst' },
-        { title: 'Resume', path: '/resume' },
-        { title: 'Contact Me', path: '/contact' },
-      ],
-      home: {
-        title: "Andrea's Portfolio",
-      },
-      about: {
-        title: 'About Me',
-      },
-      projects: {
-        title: 'Projects',
-      },
-      skills: {
-        title: 'Skills',
-      },
-      contact: {
-        title: 'Contact Me',
-      },
-    }
-  }
-  render() {
-    return (
-      <>
-        <Router>
-          <Switch>
-            <Route
-              path='/'
-              exact
-              render={() => (
-                <HomePage title={this.state.home.title} />
-              )}
-            />
-            <Route
-              path='/about'
-              render={() => (
-                <AboutMePage
-                  title={this.state.about.title}
-                />
-              )}
-            />
-            <Route
-              path='/skills'
-              render={() => (
-                <SkillsPage
-                  title={this.state.skills.title}
-                />
-              )}
-            />
-            <Route
-              path='/projects'
-              render={() => (
-                <ProjectsPage
-                  title={this.state.projects.title}
-                />
-              )}
-            />
-            <Route
-              path='/contact'
-              render={() => (
-                <ContactPage
-                  title={this.state.contact.title}
-                />
-              )}
-            />
-          </Switch>
-        </Router>
-
-        <div></div>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+      <Route path='/' exact> <HomePage /></Route>
+      <Route path='/projects' exact> <ProjectsPage title={'Projects'} /></Route>
+      <Route path='/contact' exact> <ContactPage title={'Contact Me'} /></Route>
+      <Route path='/skills' exact> <SkillsPage title={'Skills'} /></Route>
+      <Route path='/about' exact> <AboutMePage title={'About Me'} /></Route>
+      </Switch>
+    </Router>
+  )
 }
+
 
 export default App
