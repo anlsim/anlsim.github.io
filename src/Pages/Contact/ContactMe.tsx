b5c8a928a4e89c2d23fca1047aec45a840cebfc1
anlsim <anlsimental@gmail.com>
2022-11-09 19:28:15 -0800

    Added links for contanct

--- src/Pages/Contact/ContactMe.tsx
+++ src/Pages/Contact/ContactMe.tsx
@@ -1,8 +1,30 @@
 import Hero from "../../Components/Hero/Hero"
+
+import './Contact.scss'
+
 const ContactMe = () => {
   return (
     <div>
       <Hero title={'Contact Me'} />
+      <div className='contact'>
+        <div className='contact__icon'>
+          <a
+            className='contact__icon-color'
+            href='https://github.com/anlsim'
+            target='blank'>
+            <i className='fab fa-github'></i>
+          </a>
+        </div>
+
+        <div className='contact__icon'>
+          <a
+            className='contact__icon-color'
+            href='https://www.linkedin.com/in/andrea-simental/'
+            target='blank'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
